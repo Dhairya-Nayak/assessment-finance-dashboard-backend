b241ff0c39758fa7e464790d3189f149a5e02a2e
Dhairya Nayak <dhairyanayak1810@gmail.com>
2026-04-06 00:09:34 +0530

    Final push after DB correction

--- spring-boot-finance-api/src/main/java/com/financedashboard/FinanceDashboardApplication.java
+++ spring-boot-finance-api/src/main/java/com/financedashboard/FinanceDashboardApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinanceDashboardApplication {
-
+//Final push
     public static void main(String[] args) {
         SpringApplication.run(FinanceDashboardApplication.class, args);
     }
